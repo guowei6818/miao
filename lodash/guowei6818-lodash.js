a67f9c753273f6e32392e08161b87e0141665c16
guowei <614322712@qq.com>
2019-07-15 22:24:52 +0800

    time-in0715

--- lodash/guowei6818-lodash.js
+++ lodash/guowei6818-lodash.js
@@ -2,4 +2,27 @@ var guowei6818 = {
     compact: function(ary){
         return ary.filter(it => it);
     },
+    fill: function(array, value, start, end){
+        for(var i = start; i < end; i++){
+            array[i] = value;
+        }
+        return array;
+    },
+    flatten: function(array){
+        var result = [array[0]];
+        for(var i = 1; i < array.length; i++){
+            result = result.concat(array[i]);
+        }
+        return result;
+    },
+    flattenDeep: function(array){
+        var result = [array[0]];
+        for(var i = 1; i < array.length; i++){
+            if(array[i] instanceof Array){
+                array[i] = flattenDeep(array[i]);
+            }
+            result = result.concat(array[i]);
+        }
+        return result;
+    }
 }
