8ce15e6929e809590f5089b75eccc452017348e5
guowei <614322712@qq.com>
2019-07-16 21:48:47 +0800

    time-in0716

--- lodash/guowei6818-lodash.js
+++ lodash/guowei6818-lodash.js
@@ -27,5 +27,72 @@ var guowei6818 = {
             return result;
         }
         return deep(array);
+    },
+    flattenDepth: function(array, n){
+        var result = [];
+        function flatDepth(array, depth = 1){
+            for(var i = 0; i < array.length; i++){
+                if(array[i] instanceof Array){
+                    if(depth > 1){
+                        flatDepth(array[i], depth - 1);
+                    }else{
+                        result.push(array[i]);
+                    }
+                }else{
+                    result.push(array[i]);
+                }
+            }
+            return result;
+        }
+        flatDepth(array, n);
+        return result
+    },
+    negate: function(f){
+        return function(){
+            return !f();
+        }
+    },
+    bind: function(f){
+        var fixedArgs = Array.from(arguments).slice(1);
+        return function(){
+            var args = Array.from(arguments);
+            return f.apply(null, fixedArgs.concat(args));
+        }
+    },
+    flip: function(f){
+        return function(...args){
+            return f(...args.reverse());
+        }
+    },
+    before: function(func, n){
+        var lastResult
+        return function(...args){
+            while(n > 0){
+                lastResult = func(...args);
+                n--
+            }
+            return lastResult
+        }
+    },
+    ary: function(f, n = f.length){
+        return function(...args){
+            return f(...args.slice(0, n));
+        }
+    },
+    speard: function(){
+        return function(array){
+            return f(...array); 
+        }
+    },
+    memoize: function(f){
+        var map = {};
+        return function(args){
+            if(args in map){
+                return f(args)
+            }else{
+                map[args] = f[args];
+            }
+            
+        }
     }
 }
